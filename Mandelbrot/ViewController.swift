b3de27922f3eaf9eea586a1e4287e35569a150b7
ARMANDO BRIONES <armandobriones@armandos-macbook-air.local>
2016-05-11 22:47:24 -0500

    Initial Commit

---
+++ Mandelbrot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mandelbrot
+//
+//  Created by ARMANDO BRIONES on 5/11/16.
+//  Copyright © 2016 ARMANDO BRIONES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
